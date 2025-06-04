cb38ec33ed9e03934916f937a237a8cb4a14f0cd
Gustavo Koyanagui <160496975+Gutoo07@users.noreply.github.com>
2025-06-04 18:07:49 -0300

    Commit inicial

---
+++ src/main/java/fateczl/TrabalhoLabBd3/TrabalhoLabBd3Application.java
@@ -0,0 +1,13 @@
+package fateczl.TrabalhoLabBd3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoLabBd3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoLabBd3Application.class, args);
+	}
+
+}
---
+++ src/test/java/fateczl/TrabalhoLabBd3/TrabalhoLabBd3ApplicationTests.java
@@ -0,0 +1,13 @@
+package fateczl.TrabalhoLabBd3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoLabBd3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
